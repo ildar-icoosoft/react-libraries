4a60a19c90a0591de35e6e229615645e00b3d671
ildar-icoosoft <ildar.icoosoft@gmail.com>
2020-06-16 03:08:15 +0500

    add hooks (#1)

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,3 @@
-import * as React from 'react';
-
-// Delete me
-export const Thing = () => {
-  return <div>the snozzberries taste like snozzberries</div>;
-};
+export * from "./hooks/useCombinedRefs";
+export * from "./hooks/usePrevious";
+export * from "./hooks/usePreviousDifferent";
