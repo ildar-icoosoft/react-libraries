9f8996f823627d4ab407c897ff21524dfa8fa887
ildar-icoosoft <ildar.icoosoft@gmail.com>
2020-10-21 06:44:19 +0500

    fix: export useShallowEqualSelector and useDeepEqualSelector hooks (#10)

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,5 @@
 export * from "./hooks/useCombinedRefs";
 export * from "./hooks/usePrevious";
 export * from "./hooks/usePreviousDifferent";
+export * from "./hooks/useShallowEqualSelector";
+export * from "./hooks/useDeepEqualSelector";
